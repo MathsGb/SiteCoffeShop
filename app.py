3089d22e0936e90d3fc97e50d1791a24a841e5b3
MathsGb <108474535+MathsGb@users.noreply.github.com>
2023-08-06 16:35:16 -0300

    alterações

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/")
 def index():
     return render_template('index.html')
+
+app.run(debug=True)
---
+++ static/js/base.js
@@ -0,0 +1,1 @@
+exports.build = series(scsstalk, jstalk)
